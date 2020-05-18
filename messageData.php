e39666ad649b4826a61f5d41222a0c843c2c08a0
Mohamed Alyan <50428010+alyanmo96@users.noreply.github.com>
2020-05-18 06:01:53 +0300

    Add files via upload
    fix some bugs on chat files

--- messageData.php
+++ messageData.php
@@ -1,48 +1,61 @@
 <?php
+    //this file for insert a new message or export message
+
+    //start with get two id's, one for the real user, and other for the selected user form the list on messageRoom
     $userId = ($_GET['idOwner']) ? $_GET['idOwner'] : $_POST['idOwner'];
     $idOther = ($_GET['idOther']) ? $_GET['idOther'] : $_POST['idOther'];
-    $con = mysqli_connect("Localhost","id11176973_haki1","haki321","id11176973_haki");
-        
-    if($_POST['form']){
-        date_default_timezone_set('Asia/Jerusalem'); 
+
+    //connect with DB
+    $con=mysqli_connect("sql105.epizy.com","epiz_25492203","3vHHD8yqUaFf8z","epiz_25492203_Hakita");     
+    
+    if($_GET['messageVal']){//insert a new message
+        date_default_timezone_set('Asia/Jerusalem');//get the local date, for display on chat
         $script_tz = date_default_timezone_get();
         $message_date = date("y-m-d h:i");
-        $message_text=$_POST['message'];
+        $message_text=$_GET['messageVal'];//the message input
         $query="INSERT INTO `messages`(`message_sender`,`message_receive`,`message_text`,`message_date`) VALUES ('$userId',' $idOther','$message_text','$message_date')";
-        $messageResults = mysqli_query($con,$query);
+        $messageResults = mysqli_query($con,$query);//insert into DB
     }
-        $message_text=" ";
-        $message_date;   
-        $fData=" ";
-        $q='SELECT * FROM `messages` WHERE `message_receive`="'.$idOther.'" AND `message_sender`="'.$userId.'"
-        OR `message_receive`="'.$userId.'" AND `message_sender`="'.$idOther.'"';    
-        $r=mysqli_query($con,$q);		
-        if($r){
-            while($rows=mysqli_fetch_assoc($r)){                
-                $message_text=$rows['message_text'];
-                $message_date=$rows['message_date'];
-                if($rows['message_sender']==$userId){
-                    $fData.='
-                    <div class="outgoing_msg">
-                        <div class="sent_msg">
-                            <p class="msgSide">'.$message_text.'</p>
-                            <span class="time_date">'. $message_date.'</span> 
-                        </div>
-                    </div>';
-                }else{
-                    $fData.="
-                    <div class=\"incoming_msg\">
-                        <div class=\"received_msg\">
-                            <div class=\"received_withd_msg\">
-                                <p class=\"msgSide\">'. $message_text.'</p>
-                                <span class=\"time_date\">'. $message_date.'</span>
-                        </div>
+    // next section is for return the messages betweent two side, to display it
+
+    $fData=" ";//the HTML tag's and messages.      
+    $message_text=" ";//set the message as a variable
+    $message_date=" ";  //message date
+        
+    //get all message between the two side (user could be a message receive or a message sender)
+    $db='SELECT * FROM `messages` WHERE `message_receive`="'.$idOther.'" AND `message_sender`="'.$userId.'"
+    OR `message_receive`="'.$userId.'" AND `message_sender`="'.$idOther.'"'; 
+
+    $r=mysqli_query($con,$db);		
+    if($r){//if we get somthing (any message then...let $fData include it. $fData also will include tags like <div>)
+        while($rows=mysqli_fetch_assoc($r)){                
+            $message_text=$rows['message_text'];
+            $message_date=$rows['message_date'];
+            if($rows['message_sender']==$userId){
+                $fData.='
+                <div class="outgoing_msg">
+                    <div class="sent_msg">
+                        <p class="msgSide">'.$message_text.'</p>
+                        <span class="time_date">'. $message_date.'</span> 
                     </div>
-                </div>";
-                }
-            }  
-        $data = htmlentities($fData, ENT_QUOTES);
+                </div>';
+            }else{
+                $fData.="
+                <div class=\"incoming_msg\">
+                    <div class=\"received_msg\">
+                        <div class=\"received_withd_msg\">
+                            <p class=\"msgSide\">". $message_text."</p>
+                            <span class=\"time_date\">'. $message_date.'</span>
+                    </div>
+                </div>
+            </div>";
+            }
+        }//to return it as a json to convert it to a HTML code
+        header('Access-Control-Allow-Origin: *');
+        header('Content-Type: application/json');
+        $data = htmlentities($fData, ENT_QUOTES | ENT_IGNORE, "UTF-8");
         echo json_encode($fData);
     }
-exit();
+    $db->close();//close the connection
+exit();//exit from this file---like return>
 ?>
---
+++ messageRoom.php
@@ -0,0 +1,141 @@
+<?php
+//this going to be the chat page for all user also for admin
+	//start with check user validate
+	session_start();
+	$userId=$_SESSION['id'];
+	$_SESSION['id']=$userId;
+	
+	if(!$userId){//no user like this, redirect to logout file to dismiss details and redirect to main page
+		header("Location: logout.php");
+	}
+	include 'userData.php';//call userData, to use some function from
+
+    function checkId($peopleMessageArray, $id, $idOther){
+		if($id==$idOther){return -1;}
+		for($i=0;$i<count($peopleMessageArray);$i++){
+			if($peopleMessageArray[$i]==$idOther){return 1;}
+		}return -1;
+	}
+?>
+<!DOCTYPE html>
+<html>
+	<head><!--import bootstrap (help with showing{STYLE}), js for the list of cities and courses also for the up button, connect with CSS file and write the TITLE-->
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="css/profileStyle.css"><!--some addition CSS-->
+		<link rel="stylesheet" type="text/css" href="css/message.css">
+    </head>	
+    <body>
+	<section><!--navbar section--><!--navbar include the main page of the site FAQ page, EXIT-->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>        
+                <a class="navbar-brand" href="Hakita.php">הכיתה</a>
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                        <li class="nav-item active"><a class="nav-link" href="Hakita.php"> עמוד הבית</a></li>
+						<?php
+							if(checkUserDefineAs($userId)==1){
+								echo'<li class="nav-item active"><a class="nav-link" href="studentProfile.php"> פרופיל שלי</a></li>';
+							}else{
+								echo'<li class="nav-item active"><a class="nav-link" href="profile.php"> פרופיל שלי</a></li>';
+							}
+						?>
+                        <li class="nav-item active"><a class="nav-link" href="Lesson.php"> שיעורים</a></li>
+                        <li class="nav-item active"><a class="nav-link" href="FAQ.php">שאלות ותשובות</a></li>
+                        <li class="nav-item active"><a class="nav-link" href="searchTeachers.php">חיפוש מורה </a></li>             
+                        <li class="nav-item active"><a class="nav-link" href="logout.php"> יציאה<span class="sr-only"></span></a></li>
+                    </ul>
+                </div>
+          </nav>
+    </section><hr>
+    	<div class="container">
+        	<h3 class=" text-center">תיבת הודעות </h3><!--main page title-->
+            <div class="messaging">
+                <div class="inbox_msg">        
+        			<div class="mesgs">
+          				<div class="msg_history" id="msg_history"></div><!--the chat message will display here-->
+          				<div class="type_msg">
+            				<div class="input_msg_write" id="input_msg_write"></div><!--input feild-->
+          				</div>
+        			</div>
+				<div class="inbox_people">
+					<div class="inbox_chat">
+						<div class="chat_list">
+							<div class="chat_people">
+								<?php
+									//first we check on message table which messages related for this user(sent by this user, or sent for this user)
+									$peopleMessageArray=array();//array of users user talk with them
+									$peopleMessageArrayCounter=0;
+									$con=mysqli_connect("sql105.epizy.com","epiz_25492203","3vHHD8yqUaFf8z","epiz_25492203_Hakita");//DB connection
+									$messageResult=mysqli_query($con, "SELECT * FROM messages");
+									while($rows=mysqli_fetch_assoc($messageResult)){
+										if($rows['message_sender']==$userId && checkId($peopleMessageArray, $userId, $rows['message_receive'])==-1){					
+											$peopleMessageArray[$peopleMessageArrayCounter]=$rows['message_receive'];
+											$peopleMessageArrayCounter++;
+										}elseif($rows['message_receive']==$userId && checkId($peopleMessageArray, $userId, $rows['message_sender'])==-1){
+											$peopleMessageArray[$peopleMessageArrayCounter]=$rows['message_sender'];
+											$peopleMessageArrayCounter++;
+										}	
+									}
+									//display other users(talked with them as a list, on click on any user will display the chat history)
+									for($i=0;$i<$peopleMessageArrayCounter;$i++){
+										echo"<button onclick=\"getMessage($userId, $peopleMessageArray[$i])\">";
+											echo'<div class="chat_ib"><h5>';
+												echo ''.name($peopleMessageArray[$i]);
+											echo'</h5></div>
+											<div class="chat_img">';
+												echo"<img src='img/".Image($peopleMessageArray[$i])."'  class='iimg'>";
+											echo'</div>
+										</button>';
+									}
+								?>
+		</div></div></div></div></div></div></div>
+    </body>
+</html>    
+<script>  
+	var interval;//for refresh the chat history
+
+	function getData(idOwner, idOther){//get to this method from getMessage function to get the message history from messageData.php file, by sending to id's. one for the user and the second for the other user(clicked on form the list view)
+		var url = 'messageData.php';
+        $.get(url+ '?idOwner='+idOwner+'&idOther='+idOther, function(result){
+			if(result){
+                $("#msg_history").empty();//delete the message display history to display it again with a new message or display other messages
+                $('#msg_history').append(renderMessage(result));//display the new details
+            };           
+        });
+	}
+
+	function getMessage(idOwner, idOther){//after click on one of the user on the display list, will get to this function, to display the message and refresh it again.
+		clearInterval(interval); 
+		$('#input_msg_write').append(writeMessage(idOwner, idOther));
+		interval = setInterval( function() { getData(idOwner, idOther); },2000);
+	}
+
+	function renderMessage(item){//display the message history, help with append()
+		return`<div class="msg"><p>${item}</p></div>`;
+	}
+
+	function insertMessageOnTable(idOwner, idOther){//after click on send message will get to this function, to redirect information for messageData.php file to insert it on table(dB)
+		var messageVal = document.getElementById("message").value;
+		if(messageVal!=''){
+			var url = 'messageData.php';
+			$.get(url+ '?idOwner='+idOwner+'&idOther='+idOther +'&messageVal='+messageVal, function(result){
+				if(result){
+				}; 
+			});
+		}
+		$("#msg_history").empty();
+		getMessage(idOwner, idOther);//redirect to this function to display the new message
+	}
+
+	function writeMessage(idOwner,idOther){//display the field of insert message and send
+		$("#input_msg_write").empty();
+		return`<br>
+			<input type="text" id="message" autocomplete="off" autofocus placeholder="כתב/י....">
+			<input type="submit" id="messageSendButton" value="שלח/י" onclick="insertMessageOnTable(${idOwner}, ${idOther})">`;
+	}
+</script>   
